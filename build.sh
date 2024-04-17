dcb86451590215b1a11e6645512d04caa804ed36
Xi-Yuer <2214380963@qq.com>
2024-04-17 11:14:46 +0800

    git:workflow

--- build.sh
+++ build.sh
@@ -8,5 +8,9 @@ mkdir -p $BUILD_DIR
 
 # 执行打包操作
 go build -o $BUILD_DIR/your_executable ./server
+
+# 进入输出目录
+cd $BUILD_DIR || exit
+
 # 打包成压缩文件
-tar -czvf your_project_v1.0.0.tar.gz $BUILD_DIR/your_executable
+tar -czvf your_project_v1.0.0.tar.gz your_executable
