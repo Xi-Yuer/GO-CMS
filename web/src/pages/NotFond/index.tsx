a1a2aaad9edbe6518d3942653a11ed27fb33b65e
Xi-Yuer <2214380963@qq.com>
2024-05-26 15:24:50 +0800

    style:修复NotFond页面布局

--- web/src/pages/NotFond/index.tsx
+++ web/src/pages/NotFond/index.tsx
@@ -4,7 +4,7 @@ import NotFondImage from '@/assets/image/404.png';
 
 const NotFond: FC = () => {
   return (
-    <div className='flex items-center bg-white h-full dark:bg-[#121121]'>
+    <div className='flex items-center justify-center bg-white h-full dark:bg-[#121121]'>
       <Image src={NotFondImage} preview={false}></Image>
       <div className='text-6xl font-bold flex flex-col text-gray-700 dark:text-[#a6aeb3]'>
         <span>OOPS!</span>
