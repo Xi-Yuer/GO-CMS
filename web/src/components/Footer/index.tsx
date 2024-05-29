3deda857b413688e402b3bfb579767d6c5285b24
Xi-Yuer <2214380963@qq.com>
2024-05-29 10:28:35 +0800

    修改了Footer组件的备案信息，添加了许可证文件，删除了无用的导入，更新了登录页面的Logo路径。

--- web/src/components/Footer/index.tsx
+++ web/src/components/Footer/index.tsx
@@ -5,7 +5,11 @@ const Footer: FC = () => {
     <>
       <div className='flex text-[#6d727e] py-2 flex-col w-full justify-center items-center bg-[#ededed] dark:bg-[#2e2e2e] transition-all duration-150 dark:border-0 border-t border-2 border-gray-300'>
         <span>&copy; Design Xi-Yuer</span>
-        <span>备案号：20233321323</span>
+        <span>
+          <a href='https://beian.miit.gov.cn/#/Integrated/index' target='_blank'>
+            备案号： 蜀ICP备2022015920号-1
+          </a>
+        </span>
       </div>
     </>
   );
