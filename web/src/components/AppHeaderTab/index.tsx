f5adc961771b9d7116eda896d11e31cc9d2637a2
Xi-Yuer <2214380963@qq.com>
2024-06-19 17:09:12 +0800

    feat: 添加 react-transition-group 和相关动画效果

--- web/src/components/AppHeaderTab/index.tsx
+++ web/src/components/AppHeaderTab/index.tsx
@@ -3,29 +3,43 @@ import { Popover, Tag } from 'antd';
 import { Icon } from '@/components';
 import '@/styles/common/index.css';
 import { useAppHeaderTab } from '@/components/AppHeaderTab/hooks.tsx';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const AppHeaderTab: FC = () => {
   const { content, onclick, onclose, TabHeader, pathname } = useAppHeaderTab();
   return (
-    <div className='flex py-4 min-h-[55px] bg-white border-t px-8 dark:bg-[#001624] dark:border-[#a6aeb3] overflow-y-scroll no-scrollbar'>
-      {!!TabHeader.length &&
-        TabHeader?.map((item, index) => {
-          return (
-            <Popover placement='bottomLeft' key={item.pageID} trigger={'contextMenu'} content={() => content(item, index)}>
-              <Tag
-                color={pathname === item.pagePath ? '#2db7f5' : ''}
-                bordered={false}
-                icon={<Icon name={item.pageIcon as any} />}
+    <div className='flex py-4 min-h-[55px] relative bg-white border-t px-8 dark:bg-[#001624] dark:border-[#a6aeb3] overflow-y-scroll no-scrollbar'>
+      <TransitionGroup>
+        {!!TabHeader.length &&
+          TabHeader?.map((item, index) => {
+            return (
+              <CSSTransition
+                appear={true}
+                timeout={1000}
                 key={item.pageID}
-                closable={true}
-                className='cursor-pointer animate__animated animate__slideInRight select-none'
-                onClose={(e) => onclose(e, item, index)}
-                onClick={() => onclick(item)}>
-                {item.pageName}
-              </Tag>
-            </Popover>
-          );
-        })}
+                classNames={{
+                  enter: 'enter',
+                  enterActive: 'enter-active',
+                  exit: 'exit',
+                  exitActive: 'exit-active',
+                }}>
+                <Popover placement='bottomLeft' key={item.pageID} trigger={'contextMenu'} content={() => content(item, index)}>
+                  <Tag
+                    color={pathname === item.pagePath ? '#2db7f5' : ''}
+                    bordered={false}
+                    icon={<Icon name={item.pageIcon as any} />}
+                    key={item.pageID}
+                    closable={true}
+                    className='cursor-pointer select-none'
+                    onClose={(e) => onclose(e, item, index)}
+                    onClick={() => onclick(item)}>
+                    {item.pageName}
+                  </Tag>
+                </Popover>
+              </CSSTransition>
+            );
+          })}
+      </TransitionGroup>
     </div>
   );
 };
