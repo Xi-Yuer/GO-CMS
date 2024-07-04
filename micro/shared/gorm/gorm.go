b7f56462add627cc7c5cf566d6821d2d990f75f6
Xi-Yuer <2214380963@qq.com>
2024-07-04 11:01:59 +0800

    feat:引入sync.Once确保Redis和Gorm客户端初始化仅一次

--- micro/shared/gorm/gorm.go
+++ micro/shared/gorm/gorm.go
@@ -8,43 +8,47 @@ import (
 	roleModlel "micro/model/role"
 	userModel "micro/model/user"
 	"os"
+	"sync"
 	"time"
 )
 
-var DB *gorm.DB
+var (
+	DB   *gorm.DB
+	once sync.Once
+)
 
 func NewGorm() *gorm.DB {
-	if DB != nil {
-		return DB
-	}
+	once.Do(func() {
+		newLogger := logger.New(
+			log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+			logger.Config{
+				SlowThreshold:             time.Second,   // Slow SQL threshold
+				LogLevel:                  logger.Silent, // Log level
+				IgnoreRecordNotFoundError: false,         // Ignore ErrRecordNotFound error for logger
+				ParameterizedQueries:      false,         // Don't include params in the SQL log
+				Colorful:                  false,         // Disable color
+			},
+		)
+		db, err := gorm.Open(mysql.New(mysql.Config{
+			DSN:                       "root:2214380963Wx!!@tcp(127.0.0.1:3306)/micro_cms?charset=utf8&parseTime=True&loc=Local", // 数据源名称
+			DefaultStringSize:         256,                                                                                       // string 类型字段的默认长度
+			DisableDatetimePrecision:  true,                                                                                      // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
+			DontSupportRenameIndex:    true,                                                                                      // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
+			DontSupportRenameColumn:   true,                                                                                      // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
+			SkipInitializeWithVersion: false,                                                                                     // 根据当前 MySQL 版本自动配置
+		}), &gorm.Config{
+			Logger: newLogger,
+		})
 
-	newLogger := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
-		logger.Config{
-			SlowThreshold:             time.Second,   // Slow SQL threshold
-			LogLevel:                  logger.Silent, // Log level
-			IgnoreRecordNotFoundError: false,         // Ignore ErrRecordNotFound error for logger
-			ParameterizedQueries:      false,         // Don't include params in the SQL log
-			Colorful:                  false,         // Disable color
-		},
-	)
+		if err := db.AutoMigrate(&userModel.User{}, &roleModlel.Role{}); err != nil {
+			log.Fatalf("数据库迁移失败: %v", err)
+		}
+		if err != nil {
+			panic(err)
+		}
 
-	db, err := gorm.Open(mysql.New(mysql.Config{
-		DSN:                       "root:2214380963Wx!!@tcp(127.0.0.1:3306)/micro_cms?charset=utf8&parseTime=True&loc=Local", // 数据源名称
-		DefaultStringSize:         256,                                                                                       // string 类型字段的默认长度
-		DisableDatetimePrecision:  true,                                                                                      // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
-		DontSupportRenameIndex:    true,                                                                                      // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
-		DontSupportRenameColumn:   true,                                                                                      // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
-		SkipInitializeWithVersion: false,                                                                                     // 根据当前 MySQL 版本自动配置
-	}), &gorm.Config{
-		Logger: newLogger,
+		DB = db
 	})
 
-	if err := db.AutoMigrate(&userModel.User{}, &roleModlel.Role{}); err != nil {
-		return nil
-	}
-	if err != nil {
-		panic(err)
-	}
-	return db
+	return DB
 }
--- micro/shared/redis/redis.go
+++ micro/shared/redis/redis.go
@@ -4,27 +4,30 @@ import (
 	"context"
 	"github.com/redis/go-redis/v9"
 	"log"
+	"sync"
 )
 
-var redisClient *redis.Client
+var (
+	redisClient *redis.Client
+	once        sync.Once
+)
 
 // InitRedis 初始化 Redis 客户端
 func InitRedis(redisAddr string) *redis.Client {
-	// 如果 Redis 客户端已经初始化，则直接返回
-	if redisClient != nil {
-		return redisClient
-	}
+	once.Do(func() {
+		// 创建 Redis 客户端
+		redisClient = redis.NewClient(&redis.Options{
+			Addr: redisAddr,
+			// 设置连接池大小
+			PoolSize: 10,
+		})
 
-	// 创建 Redis 客户端
-	redisClient = redis.NewClient(&redis.Options{
-		Addr: redisAddr,
+		// 确保连接到 Redis
+		_, err := redisClient.Ping(context.Background()).Result()
+		if err != nil {
+			log.Fatalf("无法连接到 Redis: %v", err)
+		}
 	})
-
-	// 确保连接到 Redis
-	_, err := redisClient.Ping(context.Background()).Result()
-	if err != nil {
-		log.Fatalf("无法连接到 Redis: %v", err)
-	}
 
 	return redisClient
 }
